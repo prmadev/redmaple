f163014f9b3c39865208da704f5a3d4a036ce4af
amirography <amirhossein.alesheikh@gmail.com>
2023-01-18 11:42:27 +0330

    doc(): fix some typos in readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-//! RedMaple offers an oppinionated yet extremely flexible data modeling system based on events for backend applications
+//! RedMaple offers an opinionated yet extremely flexible data modeling system based on events for backend applications
 //!
 //! RedMaple is still in its infancy. And for now, at least, it is not fully formed.
 //! There is a 100 % certainty that if I can, I will strip away some items in it.
-//! So please, don't use it for now. Version numbering will tell you if things got stablized.
+//! So please, do not use it for now. Version numbering will tell you if things got stabilised.
 //!
 
 #[warn(clippy::pedantic)]
