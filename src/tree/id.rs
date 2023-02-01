dfae12196bd3584aaa16c9a76e0bc80a9c535ef8
amirography <amirhossein.alesheikh@gmail.com>
2023-02-01 16:24:28 +0330

    refactor: change 'uuid' method name into more idiomatic 'into_inner' method

--- src/tree/id.rs
+++ src/tree/id.rs
@@ -11,7 +11,7 @@ use uuid::Uuid;
 ///
 /// let new_id = ID::new();
 ///
-/// assert_eq!(4usize, new_id.uuid().get_version_num())
+/// assert_eq!(4usize, new_id.into_inner().get_version_num())
 ///
 /// ```
 
