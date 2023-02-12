4198d2fdbbb8299f20b8200afb1e129447445904
amirography <amirhossein.alesheikh@gmail.com>
2023-02-12 15:10:15 +0330

    doc: move tests to documentation

--- src/tree/event_group.rs
+++ src/tree/event_group.rs
@@ -7,6 +7,29 @@ use super::id::ID;
 
 /// [`EventGroup`] trait describes the behavior of an event.
 /// Specific implementaiton is not defined here
+///
+/// ```
+///    use redmaple::id::ID;
+///    use redmaple::event_group::EventGroup;
+///
+///    struct Eg(ID, ID);
+///    impl EventGroup for Eg {
+///        fn id(&self) -> &ID {
+///            &self.0
+///        }
+///
+///        fn redmaple_id(&self) -> &ID {
+///            &self.1
+///        }
+///    }
+///
+///    fn id_works() {
+///        let ev1 = Eg(ID::new(), ID::new());
+///        let ev2 = Eg(ID::new(), ID::new());
+///        assert_ne!(ev1.id(), ev2.id());
+///    }
+///
+/// ```
 pub trait EventGroup {
     /// returns the a reference to the inner ID of the event
     #[must_use]
@@ -14,4 +37,7 @@ pub trait EventGroup {
 
     /// returns the id of the parent redmaple
     fn redmaple_id(&self) -> &ID;
+
+    // TODO: add time event
+    // TODO: compare events
 }
